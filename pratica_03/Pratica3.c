085aa6a7ab5e3c110e3778bf01cda96adc8eadac
Vitor F. Carrera Miguel <62776298+VitorFCM@users.noreply.github.com>
2023-06-07 12:11:43 -0300

    Update Pratica3.c

--- pratica_03/Pratica3.c
+++ pratica_03/Pratica3.c
@@ -14,26 +14,44 @@ sbit LCD_D6_Direction at TRISB2_bit;
 sbit LCD_D7_Direction at TRISB3_bit;
 // End LCD module connections
 
-char txt1[] = "mikroElektronika";
-char txt2[] = "EasyPIC6";
-char txt3[] = "Lcd4bit";
-char txt4[] = "example";
+#define RESOLUCAO_V 0.0048876
+#define RESOLUCAO_T 0.001
 
+unsigned adc_value;
+char buff_v[12], buff_t[12];
+float resultado;
 
 void main(){
-
-  ANSELB  = 0;                        // Configure AN pins as digital I/O
+  ANSELA.f1 = 1;
+  ANSELB = 0;                        // Configure AN pins as digital I/O
+  ANSELE.f2 = 1;
+  
+  TRISA.f1  = 1;              // PORTA is input
+  TRISE.f2  = 1;              // PORTE is input
+  TRISB = 0;
 
   Lcd_Init();                        // Initialize LCD
+  Lcd_Cmd(_LCD_CLEAR);
+  Lcd_Cmd(_LCD_CURSOR_OFF);
 
-  Lcd_Cmd(_LCD_CLEAR);               // Clear display
-  Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
-  Lcd_Out(1,6,txt3);                 // Write text in first row
 
-  Lcd_Out(2,6,txt4);                 // Write text in second row
-  Lcd_Cmd(_LCD_CLEAR);               // Clear display
+  while(1){
+delay_ms(100);
+TRISA.f1  = 1;
+          //Multimetro
+          ADC_Init_Advanced(_ADC_INTERNAL_REF);
+          adc_value = ADC_Get_Sample(1);    // read analog value from ADC module channel 1
+          resultado = adc_value * RESOLUCAO_V;
+          sprintf(buff_v, "AN1 = %.2fV", resultado);
+          Lcd_Out(1,1,buff_v);
 
-  Lcd_Out(1,1,txt1);                 // Write text in first row
-  Lcd_Out(2,5,txt2);                 // Write text in second row
-
+delay_ms(100);
+TRISA.f1  = 0;
+          //Temperatura
+          ADC_Init_Advanced(_ADC_INTERNAL_FVRH1 | _ADC_INTERNAL_VREFL);
+          adc_value = ADC_Get_Sample(7);    // read analog value from ADC module channel 1
+          resultado = (adc_value * RESOLUCAO_T)/0.01;
+          sprintf(buff_t, "RE1 = %.1fC", resultado);
+          Lcd_Out(2,1,buff_t);
+  }
 }
