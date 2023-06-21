33d130f5caf199fd190452757b22dbf845d8c97d
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2023-06-21 12:04:18 -0300

    Update main.c

--- pratica_04/main.c
+++ pratica_04/main.c
@@ -15,38 +15,60 @@ sbit LCD_D7_Direction at TRISB3_bit;
 // End LCD module connections
 
 unsigned short duty;
-char buff[12];
+char buff[17];
+unsigned int count;
+char buff_rot[20];
+
+void ConfigInterrupt_Global() {
+       INTCON.GIEH = 1;
+       INTCON.GIEL = 1;
+       RCON.IPEN = 1;
+}
 
 void main() {
-       anselb = 0;
-       anseld = 0; // Porta B como digital
-       trisd = 1; // Porta B como entrada
+
+       ANSELB = 0;
+       ANSELD = 0; // Porta D como digital
+       TRISD = 0xFF; // Porta D como entrada
+       
+       ConfigInterrupt_Global();
+       
+       T3CON = 0b10000100;
+       T3CON.TMR3ON = 1;
+       
        
        Lcd_Init();                 // Initialize LCD
        Lcd_Cmd(_LCD_CLEAR);
        Lcd_Cmd(_LCD_CURSOR_OFF);
 
+       count = 0;
        duty = 0;
-       PWM1_Init(5000);                    // Initialize PWM1 module at 5KHz
+       sprintf(buff, "PWM = 0%%   ");
+       PWM1_Init(10000);                    // Initialize PWM2 module at 5KHz
+       PWM1_Start();
        while(1){
-                if(portd.rd0 == 0){
+                if (portD.f0 == 0) {
                       duty = 0;
-                }else if(portd.rd1 == 0){
+                      sprintf(buff, "PWM = 0%%   ");
+                } else if(portD.f1 == 0){
                       duty = 64;
-                }else if(portd.rd2 == 0){
+                      sprintf(buff, "PWM = 25%%  ");
+                } else if(portD.f2 == 0) {
                       duty = 127;
-                }else if(portd.rd3 == 0){
+                      sprintf(buff, "PWM = 50%%  ");
+                } else if(portD.f3 == 0) {
                       duty = 192;
-                }else if(portd.rd4 == 0){
+                      sprintf(buff, "PWM = 75%%  ");
+                } else if(portD.f4 == 0) {
                       duty = 255;
+                      sprintf(buff, "PWM = 100%% ");
                 }
                 
+                count = TMR3L;
+                sprintf(buff_rot, "ROT: %d", count);
+                Lcd_Out(2,1,buff_rot);
                 PWM1_Set_Duty(duty);
-                
-                sprintf(buff, "PWM = %d", duty);
                 Lcd_Out(1,1,buff);
-
-             //
        }
        
 }
